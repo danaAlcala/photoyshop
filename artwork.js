ec41009b2e94bc4f98ce1858c341b390218798da
danaAlcala <bigdalcala@gmail.com>
2021-03-21 17:35:37 -0700

    Update landing page

--- artwork.js
+++ artwork.js
@@ -1,128 +1,164 @@
 const ARTWORK = [
-    {
-      "Name": "The doctor is in!",
-      "Link": "https://simpleledger.info/#token/fc6a25496c479da9230fc268728a88f06f461d5dd0838eff859a8a4e734b76df",
-      "Image": "webThe_doctor_is_in",
-      "Action": "SOLD!"
-    },
-    {
-      "Name": "Told ya! Pretty cool right?",
-      "Link": "https://www.juungle.net/#/assets/7c1ee88d3eb9d89372734087bcb2ca2aa66ff06cb79bbe6f0034a43b2ae37293",
-      "Image": "webTold_ya_Pretty_cool_right",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "These blades were made for killing and that's just what they'll do...",
-      "Link": "https://www.juungle.net/#/assets/9fc80395fdce15e71a468e9e5ba7e03daf72e11d10da204a9506a0efb60f7592",
-      "Image": "webThese_blades_were_made_for_killing",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "The gang's all here.",
-      "Link": "https://www.juungle.net/#/assets/1873b2546488bc818d3852728909832e73d767a164d7bd26aaee092b52b10a0b",
-      "Image": "webThe_gangs_all_here",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Help me Obi-Wan Kenobi, you're my only hope.",
-      "Link": "https://www.juungle.net/#/assets/7db9f453748fc31cf1bdeae44ded989a2197d67b39148889a0c5c728dc4d40c4",
-      "Image": "webHelp_me_Obi_Wan_Kenobi",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Let's build a happy little Sloth!",
-      "Link": "https://www.juungle.net/#/assets/9f00e79c0eb32cabd6deaa79c5b13a54c5f4ac2012f8af6757700ff669157b1f",
-      "Image": "webLets_build_a_happy_little_Sloth",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Laugh fast!",
-      "Link": "https://www.juungle.net/#/assets/5af4c6d229904b6e2d5602e72a53963c2c76ba5f3caeaeddae233361932ec25e",
-      "Image": "webLaugh_fast",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Hell in space.",
-      "Link": "https://www.juungle.net/#/assets/7f3c658e9e2c0602a357fa7dbb48682034ca7d08a67e62b2b8c112a65021e72b",
-      "Image": "webHell_in_space",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Move over boys!",
-      "Link": "https://www.juungle.net/#/assets/3c7a8672c350c3a6ee6d86297aa77f4dd0ea2a5fb2afacdb25e1bbd450a17c9a",
-      "Image": "webMove_over_boys",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Wax off, bub!",
-      "Link": "https://www.juungle.net/#/assets/80825b4b081bf73cffa2f48ddfa93b053bc00b9c291b147f18be6b6e5a3bed6a",
-      "Image": "webWax_off_bub",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "In the thick of it.",
-      "Link": "https://www.juungle.net/#/assets/eb5d5dd888fa9adb55662cf7349952d4d29db359481e5d21283af4013dfb51f2",
-      "Image": "webIn_the_thick_of_it",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "When you have to bust yourself",
-      "Link": "https://www.juungle.net/#/assets/614b4c187d6034ba61e69a4c7c41955d436c36ec57ff38f95eb513e575cfc7f5",
-      "Image": "webWhen_you_have_to_bust_yourself",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "I live my life free of compromise, and step into the shadows without complaint or regret.",
-      "Link": "https://www.juungle.net/#/assets/160e3ca3e1e6782c0c4a09951d642abd4946fc2da3a80b4ac35a9f848c7173f6",
-      "Image": "webrorsach",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Time Traveling Wilburys",
-      "Link": "https://www.juungle.net/#/assets/71e3be9008c86d30946ce6ee551e6d8b06c6a040ac353f5082678addfb38b587",
-      "Image": "webTime_Travelling_Wilburys",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "It's raining it's pouring, Jason is goring!",
-      "Link": "https://www.juungle.net/#/assets/f531c0e0489a2545f447bb82841048e566d6e8c43108c49662947f3c2f4666d0",
-      "Image": "webIts_raining_its_pouring",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Angelic dive bomb.",
-      "Link": "https://www.juungle.net/#/assets/e91394923842c298938d7f74cc20c074365d2638c1f3290fe1109b6e79a43613",
-      "Image": "webAngelicDiveBomb",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Bitch ass narc!",
-      "Link": "https://www.juungle.net/#/assets/0001817a2a12c7bab11149dd8cfdfe01dfa1291e609c690e9510448d0365b01d",
-      "Image": "webbitchassnarc",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Bite down on this!",
-      "Link": "https://www.juungle.net/#/assets/37cfa0c6711f780a07d41a9198620f194dfc2efd96d4874531f81314f964e29c",
-      "Image": "webBiteDownOnThis",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Experiencing technical difficulties.",
-      "Link": "https://www.juungle.net/#/assets/df7c2e6428a8d53de4b8c45eecab97943b15e8776a0ceb8572ae048f3b9337d3",
-      "Image": "webExperiencingTechnicalDifficulties",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "I wanna be loved by you...",
-      "Link": "https://www.juungle.net/#/assets/40dbb907be6486c8e8b673727649cca91c3ea7843ca23a6f55f71ea6475a9770",
-      "Image": "webIWannaBeLovedByYou",
-      "Action": "Purchase with Bitcoin Cash"
-    },
-    {
-      "Name": "Totally tubular turtle Tuesday!",
-      "Link": "https://www.juungle.net/#/assets/12afdc05bb39cbe4cf4c65bed4b2baf7fdd51a139f9c1d7cd48f8d038e77468f",
-      "Image": "webTotallyTubular",
-      "Action": "Purchase with Bitcoin Cash"
-    }
-  ]
+  {
+    "Name": "The doctor is in!",
+    "Link": "https://simpleledger.info/#token/fc6a25496c479da9230fc268728a88f06f461d5dd0838eff859a8a4e734b76df",
+    "Image": "webThe_doctor_is_in",
+    "Action": "SOLD!"
+  },
+  {
+    "Name": "Told ya! Pretty cool right?",
+    "Link": "https://www.juungle.net/#/assets/7c1ee88d3eb9d89372734087bcb2ca2aa66ff06cb79bbe6f0034a43b2ae37293",
+    "Image": "webTold_ya_Pretty_cool_right",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "These blades were made for killing and that's just what they'll do...",
+    "Link": "https://www.juungle.net/#/assets/9fc80395fdce15e71a468e9e5ba7e03daf72e11d10da204a9506a0efb60f7592",
+    "Image": "webThese_blades_were_made_for_killing",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "The gang's all here.",
+    "Link": "https://www.juungle.net/#/assets/1873b2546488bc818d3852728909832e73d767a164d7bd26aaee092b52b10a0b",
+    "Image": "webThe_gangs_all_here",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Help me Obi-Wan Kenobi, you're my only hope.",
+    "Link": "https://www.juungle.net/#/assets/7db9f453748fc31cf1bdeae44ded989a2197d67b39148889a0c5c728dc4d40c4",
+    "Image": "webHelp_me_Obi_Wan_Kenobi",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Let's build a happy little Sloth!",
+    "Link": "https://www.juungle.net/#/assets/9f00e79c0eb32cabd6deaa79c5b13a54c5f4ac2012f8af6757700ff669157b1f",
+    "Image": "webLets_build_a_happy_little_Sloth",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Laugh fast!",
+    "Link": "https://www.juungle.net/#/assets/5af4c6d229904b6e2d5602e72a53963c2c76ba5f3caeaeddae233361932ec25e",
+    "Image": "webLaugh_fast",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Hell in space.",
+    "Link": "https://www.juungle.net/#/assets/7f3c658e9e2c0602a357fa7dbb48682034ca7d08a67e62b2b8c112a65021e72b",
+    "Image": "webHell_in_space",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Move over boys!",
+    "Link": "https://www.juungle.net/#/assets/3c7a8672c350c3a6ee6d86297aa77f4dd0ea2a5fb2afacdb25e1bbd450a17c9a",
+    "Image": "webMove_over_boys",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Wax off, bub!",
+    "Link": "https://www.juungle.net/#/assets/80825b4b081bf73cffa2f48ddfa93b053bc00b9c291b147f18be6b6e5a3bed6a",
+    "Image": "webWax_off_bub",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "In the thick of it.",
+    "Link": "https://www.juungle.net/#/assets/eb5d5dd888fa9adb55662cf7349952d4d29db359481e5d21283af4013dfb51f2",
+    "Image": "webIn_the_thick_of_it",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "When you have to bust yourself",
+    "Link": "https://www.juungle.net/#/assets/614b4c187d6034ba61e69a4c7c41955d436c36ec57ff38f95eb513e575cfc7f5",
+    "Image": "webWhen_you_have_to_bust_yourself",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "I live my life free of compromise, and step into the shadows without complaint or regret.",
+    "Link": "https://www.juungle.net/#/assets/160e3ca3e1e6782c0c4a09951d642abd4946fc2da3a80b4ac35a9f848c7173f6",
+    "Image": "webrorsach",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Time Traveling Wilburys",
+    "Link": "https://www.juungle.net/#/assets/71e3be9008c86d30946ce6ee551e6d8b06c6a040ac353f5082678addfb38b587",
+    "Image": "webTime_Travelling_Wilburys",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "It's raining it's pouring, Jason is goring!",
+    "Link": "https://www.juungle.net/#/assets/f531c0e0489a2545f447bb82841048e566d6e8c43108c49662947f3c2f4666d0",
+    "Image": "webIts_raining_its_pouring",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Angelic dive bomb.",
+    "Link": "https://www.juungle.net/#/assets/e91394923842c298938d7f74cc20c074365d2638c1f3290fe1109b6e79a43613",
+    "Image": "webAngelicDiveBomb",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Bitch ass narc!",
+    "Link": "https://www.juungle.net/#/assets/0001817a2a12c7bab11149dd8cfdfe01dfa1291e609c690e9510448d0365b01d",
+    "Image": "webbitchassnarc",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Bite down on this!",
+    "Link": "https://www.juungle.net/#/assets/37cfa0c6711f780a07d41a9198620f194dfc2efd96d4874531f81314f964e29c",
+    "Image": "webBiteDownOnThis",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Experiencing technical difficulties.",
+    "Link": "https://www.juungle.net/#/assets/df7c2e6428a8d53de4b8c45eecab97943b15e8776a0ceb8572ae048f3b9337d3",
+    "Image": "webExperiencingTechnicalDifficulties",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "I wanna be loved by you...",
+    "Link": "https://www.juungle.net/#/assets/40dbb907be6486c8e8b673727649cca91c3ea7843ca23a6f55f71ea6475a9770",
+    "Image": "webIWannaBeLovedByYou",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Totally tubular turtle Tuesday!",
+    "Link": "https://www.juungle.net/#/assets/12afdc05bb39cbe4cf4c65bed4b2baf7fdd51a139f9c1d7cd48f8d038e77468f",
+    "Image": "webTotallyTubular",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Conjuring a memory.",
+    "Link": "https://www.juungle.net/#/assets/c1838b0dd78630020c1df916c8f4e35e39586745300972e7a24283819330b959",
+    "Image": "webconjuringamemory",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "It has been a Sinister Sunday!",
+    "Link": "https://www.juungle.net/#/assets/546e6338f40e6bb2d80467d83c0ff35467bdd04a3e32f66211d0139e2d0d96cb",
+    "Image": "webithasbeenasinistersunday",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Laser shot this!",
+    "Link": "https://www.juungle.net/#/assets/f9bfb54074d2cd43a2ba7d3ff0fcfaa50395b449127dd3dfec0dbd321ff597e7",
+    "Image": "weblasershotthis",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "The slaughter.",
+    "Link": "https://www.juungle.net/#/assets/36eff5836c33329939956049ff185035e6b4c9b67e35f1f767180e1c5d28af78",
+    "Image": "webtheslaughter",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Thirsty",
+    "Link": "https://www.juungle.net/#/assets/14455ba012bcfb33cbc4ff8de0508f9d2739bef0a3e5123e9b774a27f88d27df",
+    "Image": "webthirsty",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Vaccinator",
+    "Link": "https://www.juungle.net/#/assets/8329bc7b6513e87959dfb2f072f4296de2f94454bb055a6f01e08f5b0a477a85",
+    "Image": "webvaccinator",
+    "Action": "Purchase with Bitcoin Cash"
+  }
+]
